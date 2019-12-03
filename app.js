b9e2799fb7e85d97e46e126aefd24a2a1242a261
Balasundaram A <53074995+balasundaram5396@users.noreply.github.com>
2019-12-03 11:44:46 -0500

    Update app.js

--- app.js
+++ app.js
@@ -12,13 +12,11 @@ var index = require('./routes/index');
 
 var app = express();
 
-// view engine setup
+
 app.set('views', path.join(__dirname, 'views'));
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -30,7 +28,6 @@ app.use(session({
 }))
 app.use(express.static(path.join(__dirname, 'public')));
 
-// res.locals is an object passed to hbs engine
 app.use(function(req, res, next) {
     res.locals.session = req.session;
     next();
@@ -45,7 +42,6 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
